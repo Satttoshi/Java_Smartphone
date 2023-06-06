b8e228b7206bc2c6ed2d72ec5dbb525e2c03e249
Josh <joshuastolle445@gmail.com>
2023-06-06 16:54:43 +0200

    add interfaces for radio and GPS

---
+++ src/main/java/org/smartphone/GPS.java
@@ -0,0 +1,5 @@
+package org.smartphone;
+
+public interface GPS {
+    int[] getPosition();
+}
---
+++ src/main/java/org/smartphone/Radio.java
@@ -0,0 +1,6 @@
+package org.smartphone;
+
+public interface Radio {
+    void startRadio();
+    void stopRadio();
+}
